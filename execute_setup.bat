0ba46a5ccc0d205130343ce5d555e444bc9dfdab
Atharv Bhaskar <atharvtech03@gmail.com>
2026-08-16 18:04:24 +0530

    Add files via upload

---
+++ execute_setup.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Navigate to the epiwatch directory
+cd /d C:\Users\silverfang\epiwatch
+
+REM Execute the Python script
+python setup_and_git.py
+
+REM Keep window open to see results
+pause
